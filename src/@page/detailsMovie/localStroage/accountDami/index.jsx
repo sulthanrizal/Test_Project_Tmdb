59f2a2d6ea0cbe89e545dbc7f8586f230fbd191f
sulthanrizal <surie0312@gmail.com>
2024-12-26 16:22:49 +0700

    craete dami for data pp

---
+++ src/@page/detailsMovie/localStroage/accountDami/index.jsx
@@ -0,0 +1,20 @@
+import Pp from './pp.png'
+
+
+export const accountDami = [
+    {
+        pp: Pp
+    },
+    {
+        pp: Pp
+    },
+    {
+        pp: Pp
+    },
+    {
+        pp: Pp
+    },
+    {
+        pp: Pp
+    },
+]
