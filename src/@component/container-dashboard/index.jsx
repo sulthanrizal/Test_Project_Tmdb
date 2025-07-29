208a040bcb9afe92409b9698189f1c5f2ebd9251
sulthanrizal <surie0312@gmail.com>
2025-07-29 19:22:43 +0700

    change slick

--- src/@component/container-dashboard/index.jsx
+++ src/@component/container-dashboard/index.jsx
@@ -1,17 +1,12 @@
 import Header from "../header";
-import './index.scss'
+import "./index.scss";
 
 const ContainerDashboard = ({ children }) => {
-
-    return (
-        <div className="container-dashboard">
-            <div className="dashboard-header">
-                <Header />
-            </div>
-            <div className="dashboard-body">
-                {children}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container-dashboard">
+      <div className="dashboard-header">{/* <Header /> */}</div>
+      <div className="dashboard-body">{children}</div>
+    </div>
+  );
+};
 export default ContainerDashboard;
--- src/@router/router.jsx
+++ src/@router/router.jsx
@@ -3,18 +3,17 @@ import Movies from "../@page/movies/movies";
 import ViewMovie from "../@page/detailsMovie";
 
 const router = [
-    {
-        path: '/',
-        element: <Home />
-    },
-    {
-        path: '/movies',
-        element: <Movies />
-    },
-    {
-        path: '/view-movie/:id',
-        element: <ViewMovie />
-    }
-
-]
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movies",
+    element: <Movies />,
+  },
+  {
+    path: "/view-movie/:id",
+    element: <ViewMovie />,
+  },
+];
 export default router;
